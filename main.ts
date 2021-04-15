1690598eee74726ba62856f12b39fb695aec22fa
haruki8ruki <82562257+haruki8ruki@users.noreply.github.com>
2021-04-15 13:53:39 +0900

    MakeCodeプロジェクトの初期ファイル

---
+++ main.ts
@@ -0,0 +1,13 @@
+basic.forever(function () {
+    basic.showIcon(IconNames.Heart)
+    basic.pause(100)
+    basic.showIcon(IconNames.SmallHeart)
+    basic.pause(100)
+    basic.showLeds(`
+        . # . # .
+        . # . # .
+        # . # . #
+        # . # . #
+        # . . . #
+        `)
+})
